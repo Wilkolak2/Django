5c7e02b5b585693ea418308bd0819d2a58aa38e0
student17 <student17@teb.pl>
2024-10-03 14:11:57 +0200

    commit 1

--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from account.forms import LoginForm
 
-# Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = LoginForm()
+    return render(request,'account/login.html', {'form': form})
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'bookmarks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'account/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -104,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl-pl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
