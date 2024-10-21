8242ab0532b070b35b1adbeaa420247bf1921097
student17 <student17@teb.pl>
2024-10-21 10:27:49 +0200

    commit 3(ten na czas)

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from account import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('login/', views.user_login, name="login")
+    # path('login/', views.user_login, name="login")
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LoginView.as_view(), name='logout'),
+
 ]
--- account/views.py
+++ account/views.py
@@ -1,24 +1,28 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from account.forms import LoginForm
 
 
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cleaned_data = form.cleaned_data
-            user = authenticate(username=cleaned_data['username'],
-                                password=cleaned_data['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse("Uwierzytelnianie zokończyło się sukcesem")
-                else:
-                    return HttpResponse("Konto zablokowane")
-            else:
-                return HttpResponse("Nieprawidłowe dane logowania")
-    else:
-        form = LoginForm()
-    return render(request,'account/login.html', {'form': form})
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cleaned_data = form.cleaned_data
+#             user = authenticate(username=cleaned_data['username'],
+#                                 password=cleaned_data['password'])
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     return HttpResponse("Uwierzytelnianie zokończyło się sukcesem")
+#                 else:
+#                     return HttpResponse("Konto zablokowane")
+#             else:
+#                 return HttpResponse("Nieprawidłowe dane logowania")
+#     else:
+#         form = LoginForm()
+#     return render(request,'account/login.html', {'form': form})
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
